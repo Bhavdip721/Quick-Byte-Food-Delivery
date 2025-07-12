f8d193de747378087056e8b7678d7dd069babadb
Bhavdip721 <bhavdipgediya717@gmail.com>
2025-07-12 01:13:25 -1100

    Delete backend directory

--- backend/routes/Cart.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addToCart, removeFromCart, getCart } from "../controllers/Cart.js";
-import authMiddleware from "../middlewares/Auth.js";
-const cartRouter = express.Router();
-//secured
-cartRouter.post("/add", authMiddleware, addToCart);
-cartRouter.post("/remove", authMiddleware, removeFromCart);
-cartRouter.post("/get", authMiddleware, getCart);
-export default cartRouter;
--- backend/routes/Contact.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { ContactUs } from "../controllers/ContactUs.js";
-
-const contactRouter = express.Router();
-
-contactRouter.post("/contact-us", ContactUs);
-
-export default contactRouter;
